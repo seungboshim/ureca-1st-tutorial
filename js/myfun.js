53053aee124c5814ea1251fa0b94fe6b1dade3f0
Seungbo Shim <fodo9898@gmail.com>
2024-07-05 15:59:08 +0900

    day0704-js

--- js/myfun.js
+++ js/myfun.js
@@ -4,7 +4,7 @@ const plus = (su1, su2) => {
         return "두 개의 수를 입력!!";
     } else if (isNaN(su1) || isNaN(su2)) {
         alert("숫자만 입력!!");
-        return;
+        return "숫자만 입력!!";
     } else {
         return parseInt(su1) + parseInt(su2);
     }
